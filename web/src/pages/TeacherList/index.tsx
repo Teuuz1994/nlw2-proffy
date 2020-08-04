09a1edd2a629c053cfea374acd69135e6a435d71
Teuuz1994 <matheus.rj.ig@gmail.com>
2020-08-04 18:02:52 -0300

    feat(frontend): added more styles

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.svg'
+import backIcon from '../../assets/icons/back.svg'
+
+import './style.css'
 
 const TeacherList: React.FC = () => {
   return (
-    <div>
-      <h1>Teacher list</h1>
+    <div id="page-teacher-list" className="contaier">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar"/>
+          </Link>
+          <img src={logoImg} alt="Proffy"/>
+        </div>
+
+        <div className="header-container">
+          <strong>Estes são os proffys disponíveis.</strong>
+        </div>
+      </header>
     </div>
   );
 };
