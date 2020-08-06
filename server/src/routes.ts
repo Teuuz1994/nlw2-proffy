a1798a7d989df7a2f6b04392defaf2a92954f905
Teuuz1994 <matheus.rj.ig@gmail.com>
2020-08-06 11:11:38 -0300

    feat(backend): create classes controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,48 +1,11 @@
 import { Router } from 'express';
 
-import db from './database/connection';
-import convertHoursInMinutes from './utils/convertHoursToMinutes';
-
 const routes = Router();
+import ClassesController from './controllers/CreateClassesController';
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
+const classesControler = new ClassesController();
 
-routes.post('/classes', async (request, response) => {
-  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
-
-  const insertUsersIds = await db('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  });
-
-  const user_id = insertUsersIds[0];
-
-  const insertedClassesIds = await db('classes').insert({
-    subject,
-    cost,
-    user_id,
-  });
-
-  const class_id = insertedClassesIds[0];
-
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: convertHoursInMinutes(scheduleItem.from),
-      to: convertHoursInMinutes(scheduleItem.to),
-    };
-  });
-
-  await db('class_schedule').insert(classSchedule);
-
-  return response.send();
-});
+routes.post('/classes', classesControler.create);
+routes.get('/classes', classesControler.index);
 
 export default routes;
